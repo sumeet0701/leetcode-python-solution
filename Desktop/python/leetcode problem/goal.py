e1ab56a42689bcf2990de5ece7d3d9a37ef0317c
sumeet <maheshwarisumeet128@gmail.com>
2023-05-23 18:48:03 +0530

    goal.py updated.

---
+++ Desktop/python/leetcode problem/goal.py
@@ -0,0 +1,17 @@
+"""
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet 
+of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string 
+"G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are
+ then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+"""
+
+def goal(command):
+    word = '()'
+    lst = command.split(word)
+    print(lst)
+
+print(goal("G()(al)"))
